f2c2bbd55bf9aec15d845f51f5aa7316fbf48904
chachacollins <collinschacha@hotmail.com>
2025-01-19 13:35:37 +0300

    feat:added licence

--- main.asm
+++ main.asm
@@ -1,3 +1,8 @@
+;; The author disclaims copyright to this source code. In place of a legal notice, here is a blessing:
+  ;       May you do good and not evil.
+  ;       May you find forgiveness for yourself and forgive others.
+  ;       May you share freely, never taking more than you give.
+
   section .data
   success_socket_create db ` Created a socket\n`
   success_socket_create_len equ $ -success_socket_create
